c6fabe2a2e4469a68fdc161db782195072033a92
Luke Beach <lukebeach@Lukes-Mac.local>
2019-06-24 02:29:12 +0530

    Added Relapse button, which prompts a confirmation. Also allowed 'a' to be a number if the moment string has 'few' in it because this means that the sentence is something like 'a few seconds ago' which makes more sense than '1 few seconds ago'

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:simple_moment/simple_moment.dart';
 import 'styles.dart';
 
 class ProgressApp extends StatelessWidget {
@@ -20,6 +22,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Variables
+  String _habitName = 'No habit :(';
+  String _counterNumber = '0';
+  String _counterDateWord = 'days ago';
+
+  // Functions
+  Future<void> setHabit() async {
+    // Grab stored habit and display it
+    final _prefs = await SharedPreferences.getInstance(); // Get stored access
+
+    DateTime lastRelapse = DateTime.parse(_prefs.getString('lastRelapse'));
+    Moment nowMoment = new Moment.fromDate(DateTime.now());
+    List<String> moments = nowMoment.from(lastRelapse).split(' ');
+    String number = moments[0];
+    String momentSubstring =
+        moments.sublist(1).toString().replaceAll(new RegExp(r"[\[,\]\,]"), '');
+    // Remove brackets and commas from string, also make 'a' = '1'
+    setState(() {
+      _habitName = _prefs.getString('habitName');
+      if (!(momentSubstring.contains('few'))) {
+        number = number.replaceAll(new RegExp(r"a"), '1');
+      }
+      _counterNumber = number; //
+      _counterDateWord = momentSubstring; // Get rest of the string
+    });
+    print(_habitName);
+  }
+
+  void relapse() async {
+    final _prefs = await SharedPreferences.getInstance(); // Get stored access
+    if (_prefs.getString('habitName') != null &&
+        _prefs.getString('lastRelapse') != null) {
+      _prefs.setString('lastRelapse', DateTime.now().toIso8601String());
+      setHabit();
+    }
+  }
+
+  _navigateAndDisplayNewHabit(BuildContext context) async {
+    final List<String> _ = await Navigator.push(
+      // Navigates to the new page, then gets the new data upon returning home
+      context,
+      CupertinoPageRoute(
+        builder: (context) => NewHabit(),
+      ),
+    );
+
+    // Redraw the habit screen
+    setState(() {
+      setHabit();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setHabit();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -32,10 +92,7 @@ class _HomePageState extends State<HomePage> {
                 right: 0,
                 child: CupertinoButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(builder: (context) => NewHabit()),
-                    );
+                    _navigateAndDisplayNewHabit(context);
                   },
                   child: Icon(
                     CupertinoIcons.ellipsis,
@@ -45,12 +102,12 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Text(
-                      'My Habit',
+                      _habitName,
                       style: Styles.counterTextString,
                     ),
                   ),
@@ -61,20 +118,82 @@ class _HomePageState extends State<HomePage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      '6',
+                      _counterNumber,
                       style: Styles.counterTextNumber,
                     ),
                     Text(
-                      'days',
+                      _counterDateWord,
                       style: Styles.counterTextString,
                     ),
                   ],
+                ),
+              ),
+              Center(
+                child: MediaQuery(
+                  data: MediaQueryData(),
+                  child: Container(
+                    margin: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height / 1.85),
+                    child: CupertinoButton(
+                      onPressed: () {
+                        showCupertinoDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return new CupertinoAlertDialog(
+                                title: Text(
+                                    'Are you sure you want to reset your progress?'),
+                                actions: <Widget>[
+                                  new CupertinoDialogAction(
+                                    child: Text('Yes, I relapsed'),
+                                    isDestructiveAction: true,
+                                    onPressed: () {
+                                      setState(() {
+                                        relapse();
+                                      });
+                                      Navigator.of(context, rootNavigator: true)
+                                          .pop();
+                                    },
+                                  ),
+                                  new CupertinoDialogAction(
+                                    child: Text('No! Keep my progress'),
+                                    isDestructiveAction: false,
+                                    onPressed: () {
+                                      Navigator.of(context, rootNavigator: true)
+                                          .pop();
+                                    },
+                                  )
+                                ],
+                              );
+                            });
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: MediaQuery.of(context).size.height / 10,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            color: CupertinoColors.activeOrange),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Relapse',
+                              style: TextStyle(
+                                  fontFamily: 'Fjalla',
+                                  color: Colors.white,
+                                  fontSize: 26),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
               MediaQuery(
                 data: MediaQueryData(),
                 child: Positioned(
                   bottom: 0,
+                  right: 0,
                   child: Image.asset(
                     'images/chart.png',
                     width: MediaQuery.of(context).size.width / 2.5,
@@ -100,11 +219,24 @@ class _NewHabitState extends State<NewHabit> {
   String _habitName;
   FocusNode _textFieldFocus = new FocusNode();
   final _habitNameController = TextEditingController();
-  
-  @override void dispose() {
+
+  @override
+  void initState() {
+    _relapseDate = DateTime.now();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
     // TODO: implement dispose
-    
+    _habitNameController.dispose();
     super.dispose();
+  }
+
+  Future<void> setHabit() async {
+    final _prefs = await SharedPreferences.getInstance();
+    _prefs.setString('habitName', _habitName);
+    _prefs.setString('lastRelapse', _relapseDate.toIso8601String());
   }
 
   @override
@@ -118,7 +250,7 @@ class _NewHabitState extends State<NewHabit> {
         ),
       ),
       child: GestureDetector(
-        onTap: (){
+        onTap: () {
           _textFieldFocus.unfocus();
         },
         child: Container(
@@ -177,9 +309,8 @@ class _NewHabitState extends State<NewHabit> {
                                 textAlign: TextAlign.left,
                                 textCapitalization: TextCapitalization.words,
                                 style: TextStyle(
-                                  fontFamily: 'SourceSans',
-                                  color: Colors.black87
-                                ),
+                                    fontFamily: 'SourceSans',
+                                    color: Colors.black87),
                               ),
                             ),
                             Padding(
@@ -199,11 +330,10 @@ class _NewHabitState extends State<NewHabit> {
                                   padding: const EdgeInsets.all(8.0),
                                   child: SizedBox(
                                     width: 300,
-                                    height: 100,
+                                    height: 125,
                                     child: CupertinoDatePicker(
                                       onDateTimeChanged: (date) {
                                         _relapseDate = date;
-                                        print(_relapseDate);
                                       },
                                       initialDateTime: DateTime.now(),
                                     ),
@@ -211,21 +341,38 @@ class _NewHabitState extends State<NewHabit> {
                                 ),
                               ],
                             ),
-                            CupertinoButton(
-                              onPressed: () {
-                                _habitName = _habitNameController.text;
-                                print("$_relapseDate $_habitName");
-                              },
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Icon(CupertinoIcons.add_circled),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 4.0),
-                                    child: Text('Confirm'),
+                            MediaQuery(
+                              data: MediaQueryData(),
+                              child: Container(
+                                margin: EdgeInsets.only(
+                                    top:
+                                        MediaQuery.of(context).size.height / 4),
+                                child: CupertinoButton(
+                                  onPressed: _habitNameController.text == ''
+                                      ? null
+                                      : () {
+                                          _habitName =
+                                              _habitNameController.text;
+                                          setHabit();
+                                          print("$_relapseDate $_habitName");
+                                          List<String> newHabit = [
+                                            _habitName,
+                                            _relapseDate.toIso8601String()
+                                          ];
+                                          Navigator.pop(context, newHabit);
+                                        },
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Icon(CupertinoIcons.add_circled),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 4.0),
+                                        child: Text('Confirm'),
+                                      ),
+                                    ],
                                   ),
-                                ],
+                                ),
                               ),
                             ),
                           ],
