a7d1fc321234ca66c8ff18e034488b095a07c038
Luke Beach <lukebeach@Lukes-Mac.local>
2019-06-23 20:57:22 +0530

    Adding custom keyboard

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
 import 'styles.dart';
 
 class ProgressApp extends StatelessWidget {
@@ -96,6 +97,31 @@ class NewHabit extends StatefulWidget {
 }
 
 class _NewHabitState extends State<NewHabit> {
+  final FocusNode _nameField = FocusNode();
+
+  KeyboardActionsConfig _buildConfig(BuildContext context) {
+    return KeyboardActionsConfig(
+      keyboardActionsPlatform: KeyboardActionsPlatform.ALL,
+      keyboardBarColor: Colors.grey[200],
+      nextFocus: true,
+      actions: [
+        KeyboardAction(
+          closeWidget: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.close),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    // Configure keyboard actions
+    FormKeyboardActions.setKeyboardActions(context, _buildConfig(context));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -133,24 +159,34 @@ class _NewHabitState extends State<NewHabit> {
                           Divider(
                             color: Colors.grey[300],
                           ),
-                          Container(
-                            margin: EdgeInsets.only(right: 300),
-                            child: Text(
-                              'Name',
-                              style: TextStyle(
-                                fontFamily: 'SourceSans'
-                              ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  'Name',
+                                  style: TextStyle(fontFamily: 'SourceSans'),
+                                ),
+                              ],
                             ),
                           ),
                           Padding(
                             padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
                             child: CupertinoTextField(
+                              keyboardType: TextInputType.text,
+                              focusNode: _nameField,
                               autocorrect: true,
+                              placeholder: 'Drinking alcohol',
+                              decoration: (BoxDecoration(
+                                color: Colors.grey[50],
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(6),
+                                ),
+                              )),
                               keyboardAppearance: Brightness.dark,
                               textAlign: TextAlign.left,
                               textCapitalization: TextCapitalization.words,
-                              style: TextStyle(
-                                  backgroundColor: CupertinoColors.white),
+                              style: TextStyle(fontFamily: 'SourceSans'),
                             ),
                           )
                         ],
